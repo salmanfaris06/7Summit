08b4ee69ed0704d439f1c50324af33641ab516fb
Chandra Rahmad Irfansyah <101537263+E-237-Chandra@users.noreply.github.com>
2023-12-25 18:56:31 +0700

    final

--- app/src/main/java/com/example/a7summit/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/a7summit/Fragment/HomeFragment.kt
@@ -1,17 +1,69 @@
 package com.example.a7summit.Fragment
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.a7summit.R
+import com.example.a7summit.databinding.FragmentHomeBinding
+import com.example.a7summit.databinding.FragmentUserBinding
+import com.google.firebase.auth.FirebaseAuth
+
 class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        val btnMerapi: Button = rootView.findViewById(R.id.btn_merapi)
+        val btnApiPurba: Button = rootView.findViewById(R.id.btn_apiPurba)
+        btnMerapi.setOnClickListener {
+            showCustomDialog()
+        }
+        btnApiPurba.setOnClickListener {
+            showCustomDialog()
+        }
+        return rootView
+    }
+
+    private fun showCustomDialog() {
+
+        val builder = AlertDialog.Builder(requireContext())
+
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.confirm_dialog, null)
+        builder.setView(dialogView)
+
+        val btnConfirm: Button = dialogView.findViewById(R.id.btn_confirm)
+
+        btnConfirm.setOnClickListener {
+            openMap()
+        }
+
+        val alertDialog = builder.create()
+        alertDialog.show()
+    }
+    private fun openMap() {
+        val latitude = -7.540561476419376
+        val longitude = 110.44570410768596
+
+        // Buat Uri untuk membuka maps
+        val gmmIntentUri = Uri.parse("geo:$latitude,$longitude?q=$latitude,$longitude")
+
+        // Intent untuk membuka aplikasi maps
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps")
+
+        if (mapIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivity(mapIntent)
+        } else {
+            Toast.makeText(requireContext(), "Aplikasi peta tidak terpasang", Toast.LENGTH_SHORT).show()
+        }
     }
 }
